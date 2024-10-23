17fd9365b8364429b06b07469c75caeea38d78dc
sedef <sedef@SEDEF>
2024-10-23 20:19:41 +0300

    Proje dosyası ekle.

---
+++ Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace yazilim231002
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("ad bilgisi giriniz");
+            string ad=Console.ReadLine();
+            Console.WriteLine("soyad bilgisi giriniz");
+            string soyad=Console.ReadLine();
+            Console.ReadKey();
+        }
+    }
+}
